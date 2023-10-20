494fb0d13c7e80cd0ea70d252d05d5be643afe49
CamilleKhv <camille.kerherve@gmail.com>
2023-10-20 14:23:34 +0200

    fin tp

--- src/test/java/ticketmachine/TicketMachineTest.java
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -30,4 +30,55 @@ class TicketMachineTest {
 		assertEquals(10 + 20, machine.getBalance(), "La balance n'est pas correctement mise à jour");              
 	}
 
+	// S3 : on n’imprime pas le ticket si le montant inséré est insuffisant
+	void NotEnoughMoney() {
+		machine.insertMoney(PRICE);
+		// Le montant inséré n'est pas suffisant
+		assertFalse(machine.printTicket(), "Le montant n'est pas suffisant");
+	}
+
+	// S4 : on imprime le ticket si le montant inséré est insuffisant
+	void EnoughMoneyTicket() {
+		machine.insertMoney(PRICE);
+		// Le montant inséré est suffisant
+		assertTrue(machine.printTicket(), "Le montant n'est pas suffisant");
+	}
+
+	// S5 : Quand on imprime un ticket la balance est décrémentée du prix du ticket
+	void balanceUpdate() {
+		machine.insertMoney(80);
+		machine.printTicket();
+		assertEquals(80 - PRICE , machine.getBalance(), "La balance a été décrémentée");
+	}
+
+	// S6 : le montant collecté est mis à jour quand on imprime un ticket (pas avant)
+	void updatedTotal() {
+		machine.insertMoney(60);
+		assertEquals(0, machine.getTotal(), "total nul");
+		machine.printTicket();
+		assertEquals(PRICE , machine.getTotal(), "Le montant a été mis à jour");
+	}
+
+	// S7 : refund() rend correctement la monnaie
+	void giveMoneyBack() {
+		machine.insertMoney(70);
+		assertEquals(70-PRICE , machine.refund(), "Le rendu monnaie est bon");
+	}
+
+	// S8 : refund() remet la balance à zéro
+	void balanceZero() {
+		assertEquals(0 , machine.getBalance(), "La balance est remise à zéro");
+	}
+
+	// S9 : on ne peut pas insérer un montant négatif
+	void noNegative() {
+		machine.insertMoney(-30);
+		assertEquals(-30 , machine.getBalance(), "Le montant ne peut pas être négatif");
+	}
+
+
+		// S10 : on ne peut pas créer de machine qui délivre des tickets dont le prix est négatif
+	void negativeNoTicket(){
+		assertThrows(IllegalArgumentException.class, () -> {new TicketMachine(-1);}, "");
+	}
 }
